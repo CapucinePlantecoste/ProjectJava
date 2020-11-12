0c5719857392c22e3aa081d10f1ccc490f147d7e
Capucine Plantecoste <capucine.plantecoste@gmail.com>
2020-11-12 18:13:24 +0100

    suppression println

--- ProjectJava/src/projectjava/ProjectJava.java
+++ ProjectJava/src/projectjava/ProjectJava.java
@@ -19,7 +19,7 @@ public class ProjectJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("salut ca va");
+        
     }
     
 }
